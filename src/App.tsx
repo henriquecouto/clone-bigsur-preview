160e5057abe6b86d43df4e9fdae8528963afb642
Henrique Couto <email@henriquecouto.com.br>
2020-10-13 14:47:51 -0300

    create global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import GlobalStyles from "./styles/global";
 
 function App() {
   return (
     <div>
+      <GlobalStyles />
       <h1>Hello Clone Big Sur Preview</h1>
     </div>
   );
