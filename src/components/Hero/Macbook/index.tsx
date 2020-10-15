2a797ae7abfea569e5c005402dbab76444b5434e
Henrique Couto <email@henriquecouto.com.br>
2020-10-15 11:55:10 -0300

    using window component directly in macbook

--- src/components/Hero/Macbook/index.tsx
+++ src/components/Hero/Macbook/index.tsx
@@ -3,12 +3,14 @@ import Desktop from "./Desktop";
 import Dock from "./Dock";
 import ControlCenter from "./ControlCenter";
 import Title from "./Title";
-import Maps from "./Maps";
-import Bitmap from "./Bitmap";
-import Safari from "./Safari";
+import Window from "./Window";
+
+import mapsImg from "../../../assets/hero_map.png";
+import safariImg from "../../../assets/hero_safari.png";
+import bitmapImg from "../../../assets/hero_bitmap.png";
 
 const Macbook: React.FC = () => {
-  const desktopRef = useRef();
+  const desktopRef = useRef(null);
 
   return (
     <>
@@ -22,9 +24,27 @@ const Macbook: React.FC = () => {
           }}
         >
           <ControlCenter />
-          <Maps desktopRef={desktopRef} />
-          <Safari desktopRef={desktopRef} />
-          <Bitmap desktopRef={desktopRef} />
+          <Window
+            desktopRef={desktopRef}
+            image={mapsImg}
+            positionX={{ start: -300, finish: 0 }}
+            positionY={{ start: -600, finish: 0 }}
+            style={{ width: 500, top: -240, left: -440 }}
+          />
+          <Window
+            desktopRef={desktopRef}
+            image={safariImg}
+            positionX={{ start: 800, finish: 0 }}
+            positionY={{ start: 600, finish: 0 }}
+            style={{ width: 510, top: -180, left: -260 }}
+          />
+          <Window
+            desktopRef={desktopRef}
+            image={bitmapImg}
+            positionX={{ start: -300, finish: 0 }}
+            positionY={{ start: 600, finish: 0 }}
+            style={{ width: 400, top: -60, left: -380 }}
+          />
         </div>
       </Desktop>
       <Title />
